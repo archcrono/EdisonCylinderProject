46ad8afba1a1cdb83da0ef9d2f57a1b9405f2e7c
Yuki Arai <sk80332@gmail.com>
2017-04-13 16:17:04 -0600

    update hidden content

--- informationSection.php
+++ informationSection.php
@@ -27,175 +27,248 @@
 
   <h1>Cylinder Project</h1>
   <p class="top__content">This website is an ongoing collective project of Utah Valley University Digital Media Students. It took a lot of different skillsets to create this website over a period of several years.</p>
-<!--   <div class="vis">
-    <img src="http://lorempixel.com/1200/200" alt="">
-  </div> -->
+
   <div class="about">
-  <div class="row">
-    <div class="col">
-      <div class="header">
-        <h2 class="header__title">Transcribing</h2>
-        <p class="header__num">01</p>
-      </div>
-      <div class="content">
-        <p>The cylinders needed to be transcribed. Over the past 7 years, with the input of Professor Mike Wisland and mechanical engineer Gary Hannah, the method of transcription has evolved over the years to the point that we now have a unique way of transcribing cylinders that is not found anywhere else. Read more information on our transcribing process activities here.</p>
-        <div id="hidden_one">
-          <div class="hidden_img">
-            <img src="/img/devices/original/original.png">  
-            <span>Figure 1. Original Assesmbly</span>
+
+    <div class="row">
+      <div class="col">
+        
+        <div class="col__img col__img--right">
+          <img src="img/cylinder1.jpg" alt="">
+        </div>
+
+        <div class="header">
+          <h2 class="header__title">Transcribing</h2>
+          <p class="header__num">01</p>
+        </div>
+
+        <div class="content">
+          <div class="paragraph">
+            The cylinders needed to be transcribed. Over the past 7 years, with the input of Professor Mike Wisland and mechanical engineer Gary Hannah, the method of transcription has evolved over the years to the point that we now have a unique way of transcribing cylinders that is not found anywhere else. Read more information on our transcribing process activities here.
           </div>
-            <p style="position:relative;">
-            First, to transcribe the cylinders electrically, we remove the original Edison playback assembly (needle and diaphragm), pictued in figure 1, and insert some form of playback device using a modern phonograph needle.  Using a Stanton 500 cartridge, we can use a wide variety of needle sizes to optimize the playback of the cylinders. The first device we had seen on the internet was a brass device that fit perfectly into the Edison mounting hole, but with a phono needle mounted directly beneath. The device was made by an unknown inventor sometime in the early 2000’s. The item was no longer available, so we borrowed the same device from the Library of Congress and had the device copied, and mounted it into an Edison Amberola 30 player, allowing electric playback. We eventually decided that the contact angle was not optimized, so we decided to modify the playback device to correct the contact angle. Those devices are pictured in figure 2.
-            <img src="/img/devices/comparison.png" style="position: absolute; width: 250px; right: -40%; bottom: 20px;"><span>Figure 2.  Old and New Playback Devices</span>
-            </p>
-          
-          <div class="hidden_img">
-            <img src="/img/devices/clear.png">  
-            <span>Figure 3. Tonearm Device</span>
-          </div>
-            <p>
+
+          <div id="hidden_one">
+            
+            <div class="hidden_img">
+              <img src="/img/devices/original/original.png">  
+              <span>Figure 1. Original Assesmbly</span>
+            </div>
+
+            <div class="paragraph">
+              First, to transcribe the cylinders electrically, we remove the original Edison playback assembly (needle and diaphragm), pictued in figure 1, and insert some form of playback device using a modern phonograph needle.  Using a Stanton 500 cartridge, we can use a wide variety of needle sizes to optimize the playback of the cylinders. The first device we had seen on the internet was a brass device that fit perfectly into the Edison mounting hole, but with a phono needle mounted directly beneath.
+            </div>
+
+            <div class="hidden_img-right">
+              <img src="/img/devices/comparison.png">
+              <span>Figure 2.  Old and New Playback Devices</span>
+            </div>
+
+            <div class="paragraph">
+              The device was made by an unknown inventor sometime in the early 2000’s. The item was no longer available, so we borrowed the same device from the Library of Congress and had the device copied, and mounted it into an Edison Amberola 30 player, allowing electric playback. We eventually decided that the contact angle was not optimized, so we decided to modify the playback device to correct the contact angle. Those devices are pictured in figure 2.
+            </div>
+            
+            
+            
+            <div class="hidden_img">
+              <img src="/img/devices/clear.png">  
+              <span>Figure 3. Tonearm Device</span>
+            </div>
+            
+            <div class="paragraph">
               After studying the devices we had created, Gary Hannah, a mechanical engineer from Kansas City, decided to approach a playback device from a completely new angle. Truly thinking outside of the circle, Gary engineered a playback device that resembles a tonearm that fits into the original Edison hole. The picture presented here in figure 3 shows how this fits into an Amberola. This allows the tonearm to float considerably more, to allow playback of out-of-round cylinders. It is a very effective device to use to try to correct skips using the first method.
-            </p>
-          <div class="hidden_img">
-            <img src="/img/devices/color/colored.png">  
-            <span>Figure 4. Back to Edison Style</span>
-          </div>
-            <p>
+            </div>
+            
+            <div class="hidden_img-right">
+              <img src="/img/devices/color/colored.png">  
+              <span>Figure 4. Back to Edison Style</span>
+            </div>
+            
+            <div class="paragraph">
               Gary was determined to create an even better transcribing device. If you’ve ever used an original Edison Amberola to play back cylinders, you know how robust that device is in playing not only out-of-round cylinders, but also its ability to play through skips. So we went back to the original Edison design to creat our 4th modification of cylinder playback. Gary carefully made measurement of the original Amerola 30 device, including the original needle and rocker arm, tied to the diaphragm with a string. We simply replaced the diapraghm with an inverted Stanton 500 cartride, but with no needle since that’s no longer necessary. It’s tied to a new needle that we ground out of a 8 mil saphire rod. Gary then 3-D printed the whole device.  Fully assembled. This is how that device looks now.
-            </p>
-            <div style="clear:both;"></div>
+            </div>            
+
             <div class="video">
               <button id="video_btn">Watch Video</button>
             </div>
+
             <div id="modal">
               <div class="modal">
                 <div class="modal_content">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/ZG1vrfoe8ns" frameborder="0" allowfullscreen></iframe>
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/ZG1vrfoe8ns" frameborder="0" allowfullscreen></iframe>
                 </div>
               </div>
             </div>
-        </div>
-        <div class="right">
-          <button id="content_1">Read More</button>
-        </div>
-        <script>
-        // show and hide Readmore contents
-          $('#content_1').click(function(){
-            $('#hidden_one').slideToggle();
-            var x = $('#content_1').text();
-            if(x == 'Read More'){
-              $('#content_1').text('Close');
-            }else{
-              $('#content_1').text('Read More');
-            }
-          });
 
-        // modal
-          $('#video_btn').click(function(){
-            var targetW = $('.modal_content iframe').width();
-            var targetH = $('.modal_content iframe').height();
-            $('.modal').css('display', 'block');
-            $('.modal_content').css({
-              'width': targetW,
-              'height': targetH
-            });
-          });
-          $(window).click(function(e){
-            var modal = $('.modal')[0];
-            if(e.target == modal){
-              $('.modal').css('display', 'none');
-            }
-          });
-        </script>
-      </div>
-    </div>
-    <div class="col col__img">
-      <img src="img/cylinder1.jpg" alt="">
-    </div>
-  </div>
-  <div class="row">
-    <div class="col col__img">
-      <img src="img/cylinder2.jpg" alt="">
-    </div>
-    <div class="col">
-      <div class="header">
-        <h2 class="header__title">Restoration</h2>
-        <p class="header__num">02</p>
-      </div>
-      <div class="content">
-        <div>
-          <p>The transcribed audio files then need to be restored. The Audio Restoration students first declick the cylinders, then denoise them before posting them to the website. We use a variety of softwares to do this, and that is an ever-evolving process. More information on who and how we restored the cylinders can be found here.</p>
-          <div id="hidden_two">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div><!-- end hidden -->
+          
+          <div class="right">
+            <button id="content_1">Read More</button>
           </div>
+
+          <script>
+          // show and hide Readmore contents
+            $('#content_1').click(function(){
+              $('#hidden_one').slideToggle();
+              var x = $('#content_1').text();
+              if(x == 'Read More'){
+                $('#content_1').text('Close');
+              }else{
+                $('#content_1').text('Read More');
+              }
+            });
+
+          // modal
+            $('#video_btn').click(function(){
+              var targetW = $('.modal_content iframe').width();
+              var targetH = $('.modal_content iframe').height();
+              $('.modal').css('display', 'block');
+              $('.modal_content').css({
+                'width': targetW,
+                'height': targetH
+              });
+            });
+            $(window).click(function(e){
+              var modal = $('.modal')[0];
+              if(e.target == modal){
+                $('.modal').css('display', 'none');
+              }
+            });
+          </script>
+
+        </div><!-- end content -->
+
+      </div><!-- end col -->
+    </div><!-- end row -->
+
+  <!-- ////////// -->
+  <!-- Restration -->
+  <!-- ////////// -->
+
+    <div class="row">
+      <div class="col">
+        
+        <div class="col__img col__img--left">
+          <img src="img/cylinder2.jpg" alt="">
+        </div>
+
+        <div class="header">
+          <h2 class="header__title">Restoration</h2>
+          <p class="header__num">02</p>
+        </div>
+
+        <div class="content">
+          <div class="paragraph">
+            The transcribed audio files then need to be restored. The Audio Restoration students first declick the cylinders, then denoise them before posting them to the website. We use a variety of softwares to do this, and that is an ever-evolving process. More information on who and how we restored the cylinders can be found here.
+          </div>
+
+          <div id="hidden_two">
+            <div class="paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          </div>
+
           <div class="right">
             <button id="content_2">Read More</button>
           </div>
+          
           <script>
-          $('#content_2').click(function(){
-            $('#hidden_two').slideToggle();
-            var x = $('#content_2').text();
-            if(x == 'Read More'){
-              $('#content_2').text('Close');
-            }else{
-              $('#content_2').text('Read More');
-            }
-          });
-        </script>
+            $('#content_2').click(function(){
+              $('#hidden_two').slideToggle();
+              var x = $('#content_2').text();
+              if(x == 'Read More'){
+                $('#content_2').text('Close');
+              }else{
+                $('#content_2').text('Read More');
+              }
+            });
+          </script>
+
+        </div>
+
+      </div>
+    </div>
+
+
+  <!-- /////////// -->
+  <!-- information -->
+  <!-- /////////// -->
+    
+    <div class="row">
+      <div class="col">
+        
+        <div class="col__img col__img--right">
+          <img src="img/cylinder3.jpg" alt="">
+        </div>
+
+        <div class="header">
+          <h2 class="header__title">Information</h2>
+          <p class="header__num">03</p>
+        </div>
+
+        <div class="content">
+          <div class="paragraph">
+            Various metadata about the cylinders is researched and collected, including a photo of the actual cylinder, Record Label, Label number, Title and Artist. If other information about the recording can be found (take number, mold number, etc), we include that in the metadata, which is displayed whenever you click on a cylinder recording.
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  <!-- /////////// -->
+  <!--   website   -->
+  <!-- /////////// -->
+
+    <div class="row">
+      <div class="col">
+
+        <div class="col__img col__img--left">
+          <img src="img/cylinder4.jpg" alt="">
+        </div>
+
+        <div class="header">
+          <h2 class="header__title">Website</h2>
+          <p class="header__num">04</p>
+        </div>
+        
+        <div class="content">
+          <div class="paragraph">
+            The creation of the website itself is the work of Mengxi Li, Jared Cooley, and Yuki Arai. The project manager who put everything together was Abe Raigne. The Design, Look, and Function was created by Yuki Arai, Jared Cooley, and Blake Stevens. Once the website code is created, then it must interface with the database of the cylinder recordings and their metadata. It was decided to separate the website code from the ever-growing database. That is technically called the website’s back end, which was designed and implemented by Blake Stevens.
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  <!-- /////////// -->
+  <!--  database   -->
+  <!-- /////////// -->
+
+    <div class="row">
+      <div class="col">
+        
+        <div class="col__img col__img--right">
+          <img src="img/cylinder2.jpg" alt="">
+        </div>
+
+        <div class="header">
+          <h2 class="header__title">Database</h2>
+          <p class="header__num">05</p>
+        </div>
+
+        <div class="content">
+          <div class="paragraph">
+            The database interface was created by Blake Stevens in MySQL. By separating the database from the actual website code, this allowed Blake to create a user interface that allows us internet dummies to add new cylinders and metadata to the database without having to know webcode. This makes updating the contents of the website very simple. The website then references the database and displays the requested content. This also allows you to search on any topic in the “metadatabase” and see the results. We sure appreciate Blake’s work….
+          </div>
         </div>
       </div>
     </div>
+
   </div>
-  <div class="row">
-    <div class="col">
-      <div class="header">
-        <h2 class="header__title">Information</h2>
-        <p class="header__num">03</p>
-      </div>
-      <div class="content">
-        <p>Various metadata about the cylinders is researched and collected, including a photo of the actual cylinder, Record Label, Label number, Title and Artist. If other information about the recording can be found (take number, mold number, etc), we include that in the metadata, which is displayed whenever you click on a cylinder recording.</p>
-      </div>
-    </div>
-    <div class="col col__img">
-      <img src="img/cylinder3.jpg" alt="">
-    </div>
-  </div>
-  <div class="row">
-    <div class="col col__img">
-      <img src="img/cylinder4.jpg" alt="">
-    </div>
-    <div class="col">
-      <div class="header">
-        <h2 class="header__title">Website</h2>
-        <p class="header__num">04</p>
-      </div>
-      <div class="content">
-        <div>
-          <p>The creation of the website itself is the work of Mengxi Li, Jared Cooley, and Yuki Arai. The project manager who put everything together was Abe Raigne. The Design, Look, and Function was created by Yuki Arai, Jared Cooley, and Blake Stevens. Once the website code is created, then it must interface with the database of the cylinder recordings and their metadata. It was decided to separate the website code from the ever-growing database. That is technically called the website’s back end, which was designed and implemented by Blake Stevens.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col">
-      <div class="header">
-        <h2 class="header__title">Database</h2>
-        <p class="header__num">05</p>
-      </div>
-      <div class="content">
-        <p>The database interface was created by Blake Stevens in MySQL. By separating the database from the actual website code, this allowed Blake to create a user interface that allows us internet dummies to add new cylinders and metadata to the database without having to know webcode. This makes updating the contents of the website very simple. The website then references the database and displays the requested content. This also allows you to search on any topic in the “metadatabase” and see the results. We sure appreciate Blake’s work….</p>
-      </div>
-    </div>
-    <div class="col col__img">
-      <img src="img/cylinder2.jpg" alt="">
-    </div>
-  </div>
-  </div>
+
+<!-- /////////// -->
+<!--   members   -->
+<!-- /////////// -->
+
   <h1>Project Members</h1>
   <div class="members">
     <div class="member">
