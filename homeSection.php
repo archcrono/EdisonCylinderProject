d4b9773c1986622bf9b0b0bdac9d2f8fb0efb2ed
Jared Cooley <archcrono@gmail.com>
2017-04-18 17:45:28 -0600

    Implemented Lazy Load Added to Home and About pages for testing Fails to load untill you resize the screen, somthing is interfearing with the script regestering the viewport.

--- homeSection.php
+++ homeSection.php
@@ -4,7 +4,7 @@
   <div id="homeContainer">   
 
       <div class="homeBoxes">
-      <img class="alignleft" src="img/lib.jpg">
+      <img class="alignleft lazy" src="images/grey.gif" data-original="img/lib.jpg">
         <span>
           <h2 class="blue">Listen to the Library!</h2>
           <p>Duis non urna augue. Donec eu mauris ante. Curabitur sem enim, porta a venenatis sed, consectetur in massa. Aenean pellentesque rutrum nisl, non consequat ligula suscipit eu. Proin viverra id purus id tempus. Cras tortor diam, scelerisque nec elit quis, blandit scelerisque orci. Mauris faucibus sodales pharetra. Cras quis ligula vel lorem tincidunt euismod id eu magna. Aliquam interdum, magna in ultrices congue, felis nisl sollicitudin justo, vel aliquet arcu arcu quis velit. Vestibulum pellentesque lobortis velit ut aliquam. Aliquam tempor porttitor velit, vitae tristique nisi consequat ut. Curabitur a neque ac nisi varius ornare. </p>
@@ -15,7 +15,7 @@
       </div>
  
       <div class="homeBoxes">
-        <img class="alignleft" src="img/month.jpg">
+        <img class="alignleft lazy" src="images/grey.gif" data-original="img/month.jpg">
         <span>
           <h2 class="red">This Month In History!</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempus sit amet lectus ut tincidunt. Vivamus dignissim fermentum nisl in fringilla. Etiam felis orci, malesuada nec turpis quis, tempor sollicitudin libero. Cras laoreet congue venenatis. Donec dui sapien, sodales eget maximus nec, tempor a odio. Curabitur nisi est, sollicitudin vitae posuere ac, volutpat id dui. Nunc id accumsan leo, vel pharetra lectus. Nunc rutrum id orci ac semper. </p>
@@ -26,7 +26,7 @@
       </div>
 
       <div class="homeBoxes">
-      <img class="alignleft" src="img/donate.jpg">
+      <img class="alignleft lazy" src="images/grey.gif" data-original="img/donate.jpg">
         <span>
           <h2 class="yellow">Donate!</h2>
           <p>Maecenas eget dignissim lacus, vitae posuere mauris. Donec at elit ligula. Nunc in sapien quis velit varius iaculis. Vestibulum nec massa nec lorem pellentesque laoreet quis quis lacus. Donec sit amet lacinia tellus. Nulla congue accumsan massa sed luctus. Morbi quis enim rutrum, luctus risus sed, condimentum felis. Quisque urna felis, pulvinar in sagittis ac, pharetra ac nulla. Curabitur laoreet erat non sapien aliquet malesuada. Maecenas facilisis felis vel lorem mollis, sed mollis est ullamcorper. Maecenas quis felis consequat, condimentum odio in, luctus dui. Mauris quis mi dapibus, congue ex eget, consequat justo. Donec eget faucibus nibh. Quisque blandit ac tellus quis pretium. </p>
